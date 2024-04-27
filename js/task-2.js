374b3b1979525887e6aa052528fd121645a7d067
olhagrytsiuk <oljazozylja@gmail.com>
2024-04-28 01:39:06 +0300

    home work

---
+++ js/task-2.js
@@ -0,0 +1,34 @@
+class Storage {
+  #items;
+  constructor(params) {
+    this.#items = params;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    for (const key in this.#items) {
+      if (this.#items[key].includes(itemToRemove)) {
+        this.#items.splice(
+          this.#items.indexOf(itemToRemove),
+          this.#items.indexOf(itemToRemove)
+        );
+      }
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
